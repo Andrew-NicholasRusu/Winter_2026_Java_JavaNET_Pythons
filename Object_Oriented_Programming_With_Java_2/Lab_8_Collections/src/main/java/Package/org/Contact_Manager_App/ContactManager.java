9f1b1bb5e5e2a15bbffed213081f199c6a40f40e
Andrew-Nicholas Rusu <andrewnicholasrusu@gmail.com>
2026-05-20 11:13:55 -0400

    Implement Printable interface in ContactManager class

--- Object_Oriented_Programming_With_Java_2/Lab_8_Collections/src/main/java/Package/org/Contact_Manager_App/ContactManager.java
+++ Object_Oriented_Programming_With_Java_2/Lab_8_Collections/src/main/java/Package/org/Contact_Manager_App/ContactManager.java
@@ -3,11 +3,11 @@ package Package.org.Contact_Manager_App;
 import java.util.Collection;
 
 // c) Define an abstract class
-public abstract class ContactManager {
-
+public abstract class ContactManager implements Printable {  
+    
     public abstract void addContact(String id, String name, String mobile, String email);
     public abstract void addContact(Contact contact);
     public abstract Contact getContactByEmail(String email);
     public abstract Collection<Contact> getContactsByAreaCode(String areaCode);
-    public abstract void printAll();
+    public abstract boolean removeContact(String id);
 }
