64a9e872417bd2d66562e70b6503f53aba2daade
Andrew-Nicholas Rusu <andrewnicholasrusu@gmail.com>
2026-05-20 11:10:57 -0400

    Refactor m_methodClass to Lab10P1 and improve logic

--- Object_Oriented_Programming_With_Java_2/Lab_10_Recursion/src/m_methodClass.java
+++ Object_Oriented_Programming_With_Java_2/Lab_10_Recursion/src/m_methodClass.java
@@ -3,20 +3,25 @@
  * m(i) = 1 + 1 / 2 + 1 / 3 + 1 / 4 ... +1/i.,.,
  */
 
-public class m_methodClass {
+public class Lab10P1 {
 
     public static void main(String[] args) {
 
-        double result = m_method(10);
-        System.out.printf("Result of m_method(%d) is %f \n", 10, result);
+        for (int i = 1; i <= 10; i++) {
+        System.out.printf("m(%d) = %.2f\n", i, m_method(i));
+        }
     }
 
     public static double m_method(int i) {
-        double result =- 1;
-        if (i <= 0) // error case
+        double result;
+        if (i <= 0){ // error case
             System.out.println("m_method is not defined for " + i);
-        else if (i == 1) // base case
-            result = 1;
+            result = 0;
+        }
+        else if (i == 1){ // base case
+            result = 1.0;
+        }
+        // recursive case
         else {
             System.out.printf("calling m_method(%d) \n", i - 1);
             result = m_method(i - 1) + (1.0 / i); // m_method (i - 1) is the recursive call
